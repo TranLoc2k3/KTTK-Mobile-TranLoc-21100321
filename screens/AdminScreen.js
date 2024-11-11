f4f0525fc19dff2f89767e77c10f5944f4150b8a
TranLoc2k3 <tranloc120603@gmail.com>
2024-11-11 13:13:31 +0700

    init project

---
+++ screens/AdminScreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AdminScreen = () => {
+  return (
+    <Text> AdminScreen</Text>
+  )
+}
---
+++ screens/IntroduceScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, Text, Button} from 'react-native'
+
+export const IntroduceScreen = ({navigation}) => {
+  return (
+    <View>
+        <Text>IntroduceScreen</Text>
+       
+     </View>
+  )
+}
