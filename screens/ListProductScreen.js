c7871adb29a32a68b1e3c90868286627dab84698
TranLoc2k3 <tranloc120603@gmail.com>
2024-11-11 14:04:06 +0700

    Add result of ListProductScreen and Product Detail

--- screens/ListProductScreen.js
+++ screens/ListProductScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, FlatList, StyleSheet, Text, StatusBar, Image } from 'react-native';
+import { View, FlatList, StyleSheet, Text, StatusBar, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
   {
@@ -7,47 +8,58 @@ const data = [
     name: "Pinarello",
     price: 1500,
     image: "https://i.ibb.co/bBktyF1/image.png",
+    description: "It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc.",
   },
   {
     id: 2,
     name: "Specialized",
     price: 2000,
     image: "https://i.ibb.co/bBktyF1/image.png",
+    description: "It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc.",
   },
   {
     id: 3,
     name: "Cannondale",
     price: 3000,
     image: "https://i.ibb.co/bBktyF1/image.png",
+    description: "It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc.",
   },
   {
     id: 4,
     name: "Bianchi",
     price: 2500,
     image: "https://i.ibb.co/bBktyF1/image.png",
+    description: "It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc.",
   },
   {
     id: 5,
     name: "Trek",
     price: 2800,
     image: "https://i.ibb.co/bBktyF1/image.png",
+    description: "It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc.",
   },
   {
     id: 6,
     name: "ABS",
     price: 2800,
     image: "https://i.ibb.co/bBktyF1/image.png",
+    description: "It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc.",
   },
 ];
 
 export const ListProductScreen = () => {
 
+  const navigation = useNavigation();
+
   const renderItem = ({ item }) => (
-    <View style={styles.itemContainer}>
-      <Image style={{height: 50, width: 50}} source={{ uri: item.image }}  />
+    <TouchableOpacity
+      style={styles.itemContainer}
+      onPress={() => navigation.navigate('ProductDetail', { item })}
+    >
+      <Image style={{ height: 50, width: 50 }} source={{ uri: item.image }} />
       <Text style={styles.name}>{item.name}</Text>
       <Text style={styles.price}>${item.price}</Text>
-    </View>
+    </TouchableOpacity>
   );
 
   return (
--- screens/ProductDetail.js
+++ screens/ProductDetail.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 export const ProductDetail = () => {
+  const route = useRoute();
+  const { item } = route.params;
+
   return (
-    <Text> ProductDetail</Text>
-  )
-}
+    <View style={styles.container}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <View>
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.price}>${item.price}</Text>
+      </View>
+      <Text style={styles.description}>{item.description}</Text>
+      <Button title="Click to buy" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    padding: 16,
+  },
+  image: { width: '100%', height: 200, marginBottom: 20, resizeMode: 'cover' },
+  name: { fontSize: 36, fontWeight: 'bold', marginBottom: 10 },
+  price: { fontSize: 24, color: '#888' },
+  description: { fontSize: 18 },
+});
